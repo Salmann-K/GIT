13f419736abec2c89ebb9aeb81a18dd577b448ba
Salman <salmanprof9@gmail.com>
2022-04-17 09:23:28 +0530

    Added Multilpy.java

---
+++ Multiply.java
@@ -0,0 +1,9 @@
+package com.company.dsajava.GIT;
+
+public class Multiply {
+    public static void main(String[] args) {
+        int a=9,b=10;
+
+        System.out.println("The Product Is " + a*b);
+    }
+}
