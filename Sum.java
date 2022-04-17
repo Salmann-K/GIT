af647d7d14c7f097a515932fde5bcdff71b37ef4
Salman <salmanprof9@gmail.com>
2022-04-17 09:01:19 +0530

    Initial Commit

---
+++ Sum.java
@@ -0,0 +1,9 @@
+package com.company.dsajava.GIT;
+
+public class Sum {
+    public static void main(String[] args) {
+        int a=9,b=10;
+        int c=a+b;
+        System.out.println(c);
+    }
+}
