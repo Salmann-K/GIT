423df88c1e5a0ce06dfc5ef98fc3e9ce4f6dcece
Salman <salmanprof9@gmail.com>
2022-04-17 09:43:07 +0530

    New Implementation

--- Diff.java
+++ Diff.java
@@ -3,7 +3,6 @@ package com.company.dsajava.GIT;
 public class Diff {
     public static void main(String[] args) {
         int a=15,b=1;
-        int c=a-b;
-        System.out.println(c);
+        
     }
 }
