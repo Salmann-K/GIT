cac38b76bb9fc65b8ec0bd2304d639104da3f87b
Salman <salmanprof9@gmail.com>
2022-04-17 09:13:00 +0530

    Added Message In Sum.java

--- Sum.java
+++ Sum.java
@@ -3,7 +3,7 @@ package com.company.dsajava.GIT;
 public class Sum {
     public static void main(String[] args) {
         int a=9,b=10;
-        int c=a+b;
-        System.out.println(c);
+        int c=a-b;
+        System.out.println("The Sum Is " + c);
     }
 }
